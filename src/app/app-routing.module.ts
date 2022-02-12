f2492d7050b762145ce7441edd863a738746a16b
Zaid Quadri <Zaid.Quadri@pearson.com>
2022-02-12 13:11:29 -0500

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './pages/blog/blog.component';
 import { HomeComponent } from './pages/home/home.component';
-const routes: Routes = [{ path: '', component: HomeComponent }];
+import { ProjectsComponent } from './pages/projects/projects.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blogs', component: BlogComponent },
+  { path: 'projects', component: ProjectsComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavbarComponent, FooterComponent],
 })
 export class SharedModule {}
